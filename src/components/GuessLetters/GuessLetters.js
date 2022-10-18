355add343467fb336d66a172517a27cf8be9432b
Enric Vazquez <vspevc@gmail.com>
2022-10-18 23:24:56 +0200

    Add dynamic guess letters

--- src/components/GuessLetters/GuessLetters.js
+++ src/components/GuessLetters/GuessLetters.js
@@ -3,11 +3,17 @@ import "./GuessLetters.css";
 const GuessLetters = ({ guessLetters }) => {
   return (
     <ul className="guess-letters">
-      <li className="guess-letter empty"></li>
-      <li className="guess-letter">A</li>
-      <li className="guess-letter empty"></li>
-      <li className="guess-letter">A</li>
-      <li className="guess-letter empty"></li>
+      {guessLetters.map((guessLetter, key) => {
+        if (guessLetter.hidden) {
+          return <li key={key} className="guess-letter empty"></li>;
+        }
+
+        return (
+          <li key={key} className="guess-letter">
+            {guessLetter.letter}
+          </li>
+        );
+      })}
     </ul>
   );
 };
